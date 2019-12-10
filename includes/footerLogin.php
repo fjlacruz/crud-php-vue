dfc648efa7d5e7be1d1622dadaa3520c372fc54b
idsistemas15 <idsistemas15@gmail.com>
2019-12-09 21:04:32 -0300

    crud php vue

---
+++ includes/footerLogin.php
@@ -0,0 +1,7 @@
+</main>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="../js/appLogin.js"></script>
+</body>
+</html>
